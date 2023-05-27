980b96261a3f265137015172fb0c4d5c5c95303f
Andrei Agdo <andrei@agdo.com.br>
2023-05-26 22:12:39 -0300

    instalação api

--- apps/sigma-api/src/main.ts
+++ apps/sigma-api/src/main.ts
@@ -1,15 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import compression from '@fastify/compress';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,
+    new FastifyAdapter());
+  await app.register(compression, { encodings: ['br', 'gzip', 'deflate', 'identity'] });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
